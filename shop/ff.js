d75a6ec900a019a5bc082fb766ff1da27fd848d8
sulovens <sasra.abdi8@gmail.com>
2022-01-22 06:19:19 +0700

    p

--- shop/ff.js
+++ shop/ff.js
@@ -1,16 +1,16 @@
 const epep = `
  *「DIAMOND FREEFIRE」*
 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-👻> *10 DIAMOND = Rp 2.000*
-👻> *20 DIAMOND = Rp 5.000*
 👻> *50 DIAMOND = Rp 8.000*
 👻> *70 DIAMOND = Rp 10.000*
-👻> *100 DIAMOND = Rp 15.000*
-👻> *120 DIAMOND = Rp 18.000*
 👻> *140 DIAMOND = Rp 20.000*
 👻> *210 DIAMOND = Rp 30.000*
 👻> *280 DIAMOND = Rp 40.000*
+👻> *355 DIAMOND = Rp 48.000*
+👻> *425 DIAMOND = Rp 57.000*
+👻> *495 DIAMOND = Rp 66.000*
 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 *NOTE :*
-*_JIKA MINAT BISA HUBUNGI OWNER_*`
+*_JIKA MINAT BISA HUBUNGI OWNER_*
+_IG: *dtm.gstore* _ `
 exports.epep = epep
