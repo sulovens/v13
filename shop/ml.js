02086a00df468b6fb9bf3325e41f4934ded45b6c
sulovens <sasra.abdi8@gmail.com>
2022-01-22 06:30:41 +0700

    p

---
+++ shop/ml.js
@@ -0,0 +1,16 @@
+const ml = `
+ *「DIAMOND MOBILE LEGENDS」*
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+👻> *86 DIAMOND = Rp 20.000*
+👻> *172 DIAMOND = Rp 40.000*
+👻> *257 DIAMOND = Rp 60.000*
+👻> *344 DIAMOND = Rp 80.000*
+👻> *429 DIAMOND = Rp 100.000*
+👻> *514 DIAMOND = Rp 120.000*
+👻> *600 DIAMOND = Rp 140.000*
+👻> *706 DIAMOND = Rp 160.000*
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+*NOTE :*
+*_JIKA MINAT BISA HUBUNGI OWNER_*
+_IG: *dtm.gstore* _ `
+exports.ml = ml
